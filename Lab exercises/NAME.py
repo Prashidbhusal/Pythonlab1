db39aac091732342aba06f4be151f836c8ec029e
ASUS <prashidbhusal3000@gmail.com>
2021-02-23 12:08:35 +0545

    Mynameprashid

--- Lab exercises/NAME.py
+++ Lab exercises/NAME.py
@@ -1,6 +1,6 @@
 # My name is Prashid ,and i am 20 years old.
 
-name=str("Prashid")
+name=str(input("enter your name:"))
 age=int(input("enter age"))
 
 print(" my name is " + name + " and i am " + str (age) + " years old ")
