544871d37e65055d70606c95f8426aec22c9363b
ASUS <prashidbhusal3000@gmail.com>
2021-02-23 11:39:49 +0545

    LAB1.0

---
+++ Lab exercises/Area of right.py
@@ -0,0 +1,11 @@
+# Write a program that reads the length of the base and the height of a right-angled tringle and prints the area.
+# Every number is given on a separete line.
+
+length=int(input("enter the value of length: "))
+height=int(input("enter the height: "))
+
+area=(length * height)//2
+
+#the floor division // rounds that result down to the nearest whole number
+
+print(f"The area of right angled triangle is {area} ")
