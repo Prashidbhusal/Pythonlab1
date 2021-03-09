c261e77351bca35bf2904c125e5e8c294cdd74fd
ASUS <prashidbhusal3000@gmail.com>
2021-03-09 12:15:32 +0545

    first commit

---
+++ Lab exercises/question 9.py
@@ -0,0 +1,4 @@
+#Write a python program which accepts the radius of circle from the user and compute the area.(area of circle=Pi*r^2)
+radius=float(input('enter the radius of a circle'))
+area=3.14*radius**2
+print(f'the area of circle is {area} square meter.')
