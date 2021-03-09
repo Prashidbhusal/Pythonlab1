4dcfdb1c9d1fe6fe6fc101df965edf8ed01c2092
ASUS <prashidbhusal3000@gmail.com>
2021-03-09 12:24:32 +0545

    initial commit

---
+++ Lab exercises/question 10.py
@@ -0,0 +1,6 @@
+#Write a program to find sum of the first sum of the first n position integers.
+#sum=(n*(n+1))/2
+n=float(input('enter a positive integer'))
+sum=(n*(n+1))/2
+print(f'the sum of the positive integer is {sum}')
+
