634872419d82cb08ce3b0b01023b496e873db9b6
ASUS <prashidbhusal3000@gmail.com>
2021-03-30 12:22:39 +0545

    kg to lbs

--- Lab exercise 2/QUESTION 5.py
+++ Lab exercise 2/QUESTION 5.py
@@ -1,2 +1,10 @@
 #IF NAME IS LESS THEN  3 CHARACTERS OTHERWISE IF IT'S MORE THEN 50 CHARACTERS - NAME MUST BE MAXIMUM OF 50
 #OTHERWISE - NAME LOOKS GOOD!
+
+name=str(input('Enter the name:'))
+if (len(name))<=3:
+    print('name must be 3 character')
+elif len(name)>=50:
+    print('name must be less then 50')
+else:
+    print('hamre chutiya banawe')
---
+++ Lab exercise 2/QUESTION 6.py
@@ -0,0 +1,6 @@
+#Weight converter:
+#Input the weight of a person provides his/her weight in kg then convert it into lbs else convert it to kg.
+
+weight = float(input('Enter weight in kilo_gram to Convert into lbs:'))
+lbs= weight* 2.20426
+print(weight,' Pounds (Lbs) are equal to',lbs,'Kilograms (Kgs)')
