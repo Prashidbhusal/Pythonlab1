c548109cd98960d7003b4894418a54f3e192e34d
ASUS <prashidbhusal3000@gmail.com>
2021-04-17 17:32:33 +0545

    lab 1

--- lab1/Lab exercise 2/QUESTION 15.py
+++ lab1/Lab exercise 2/QUESTION 15.py
@@ -1,7 +1,7 @@
 #Check weather the given year is leap year or not .If year is leap print 'LEAP YEAR' else print 'COMMON YEAR'
 #HINT: a year is a leap year if its number is exactely divisible by 4 and is not exactly divisible by 100
 #    : a year is always a leap year if its number is exactely divisible by 400.
-year = int(input("Enter a year: "))
+'''year = int(input("Enter a year: "))
 if (year % 4) == 0:
    if (year % 100) == 0:
        if (year % 400) == 0:
@@ -11,4 +11,9 @@ if (year % 4) == 0:
    else:
        print("{0} IS LEAP YEAR".format(year))
 else:
-   print("{0} IS COMMON YEAR".format(year))
+   print("{0} IS COMMON YEAR".format(year))'''
+
+year=int(input('enter year to be checked'))
+if(year%4==0 and year % 100 !=0 or year % 400 ==0):
+else:
+    print("The year isn't a leap year!")
---
+++ lab1/Lab exercise 2/QUESTION 16.py
@@ -0,0 +1,9 @@
+#Write a Python program to sum of three given integers. However ,if two values are equal sum will be zero.
+def sum(a,b,c):
+    if a==b or b==c or c==a:
+        sum=0;
+    else:
+        sum=(a+b+c)
+        return sum
+x,y,z =[int(a) for a in input("enter three number:").split(",")]
+print(sum(x,y,z))
